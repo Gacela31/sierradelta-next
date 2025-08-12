97faaf41b3431e7789ac4c7da13c6538d51a41db
Gimena <gimena.sanchezvil@gmail.com>
2025-08-12 19:49:45 -0300

    style: brand palette (#1F3B49) + helpers and tailwind glob fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,7 @@ const config: Config = {
   content: [
     './app/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
-    './messages/**/*.{json}'
+    './messages/**/*.json'
   ],
   theme: {
     extend: {},
