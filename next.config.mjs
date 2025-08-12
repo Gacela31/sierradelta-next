fc69afb96788c8d81bb60c973640babd0c2dcfaa
Gimena <gimena.sanchezvil@gmail.com>
2025-08-12 12:09:29 -0300

    chore: disable typedRoutes to fix Link href types

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,11 @@
 // next.config.mjs
 import createNextIntlPlugin from 'next-intl/plugin';
-
-// Conecta next-intl con tu config de mensajes
 const withNextIntl = createNextIntlPlugin('./i18n.ts');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    typedRoutes: true
-  }
+  reactStrictMode: true
+  // 👇 Quitamos experimental.typedRoutes
 };
 
 export default withNextIntl(nextConfig);
